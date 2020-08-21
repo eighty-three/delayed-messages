042dabf6ace89f4d3f37da6695fbf31531b7293b
Eighty Three <cwp1@protonmail.com>
2020-08-21 13:33:46 +0800

    Add configs for testing React apps

---
+++ client/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/styleMock.js',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  roots: ['<rootDir>/src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  setupFilesAfterEnv: [
+    '<rootDir>/setupTests.js',
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+};
